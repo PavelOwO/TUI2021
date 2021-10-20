acb7c882f18094d4c9dc0be8ab5f77e97044150f
UnforgivenLalalend <acc4anim@gmail.com>
2021-10-20 11:45:50 +0300

    send data from front to server

--- back/server/server.py
+++ back/server/server.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route("/", methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        print(request.form)
+    return render_template('test.html')
